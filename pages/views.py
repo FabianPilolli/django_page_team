d1641f48e2bd55ac4109ec15a627cf315fa2556f
Fabian <fabianejp1@gmail.com>
2023-06-10 17:16:57 -0400

    #7-MVTU Update

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("Hola Mundo")
+class HomePageView(TemplateView):
+    template_name = "home.html"
