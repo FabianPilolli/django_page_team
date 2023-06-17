6a992ee55423c7ed70d3b80ca359fef19b175450
Miguelangel Avila <miguelangel.a1305@gmail.com>
2023-06-17 16:06:28 -0400

    Crear articulos

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostCreateView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-
+    path('create_post/', PostCreateView.as_view(), name="create_post")
    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy 
+from .models import Post
 
 # Create your views here.
+
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "create_post.html"
+    fields = ('title', 'description')
+    success_url = reverse_lazy('home')
