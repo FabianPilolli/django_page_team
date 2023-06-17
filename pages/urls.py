bac66f5989d4dc271dafa9511b07963cea0fd46f
Miguelangel Avila <miguelangel.a1305@gmail.com>
2023-06-17 18:06:26 -0400

    Actualizar articulos

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomePageView, PostCreateView, PostListView
+from .views import HomePageView, PostCreateView, PostListView,PostDetailView, PostUpdateView
 
 urlpatterns = [
     
     path('create_post/', PostCreateView.as_view(), name="create_post"),
     path('', PostListView.as_view(), name="post_list"),
+    path('post/<int:pk>', PostDetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(),name='post_update')
+
    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView,DetailView, UpdateView
 from django.urls import reverse_lazy 
 from .models import Post
 
@@ -16,4 +16,15 @@ class PostCreateView(CreateView):
 class PostListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'description')
+    template_name = 'update_post.html'
+    success_url = reverse_lazy('post_list')
+
 
